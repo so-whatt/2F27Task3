aee3497d83f0a872a042f63fd75fb3fb88588fe2
so-whatt <115199035+so-whatt@users.noreply.github.com>
2022-10-07 09:39:42 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,12 +3,63 @@ def on_button_pressed_a():
     for index in range(6):
         basic.show_number(count)
         basic.pause(1000)
-        count += 1
+        count += -1
+    basic.show_number(0)
+    basic.clear_screen()
+    music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+            4500,
+            0,
+            255,
+            0,
+            3500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        SoundExpressionPlayMode.UNTIL_DONE)
+    basic.pause(500)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+    """)
+    basic.show_leds("""
+        . . . . .
+                . . # . .
+                . # . # .
+                . . # . .
+                . . . . .
+    """)
+    basic.show_leds("""
+        . . # . .
+                . # . # .
+                # . . . #
+                . # . # .
+                . . # . .
+    """)
+    basic.show_leds("""
+        . # . # .
+                # . . . #
+                . . . . .
+                # . . . #
+                . # . # .
+    """)
+    basic.show_leds("""
+        # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+    """)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+    basic.pause(1000)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 count = 0
-count = 1
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+count = 6
--- main.ts
+++ main.ts
@@ -2,55 +2,8 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 6; index++) {
         basic.showNumber(count)
         basic.pause(1000)
-        count += -1
+        count += 1
     }
-    basic.showNumber(0)
-    basic.clearScreen()
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 4500, 0, 255, 0, 3500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    basic.pause(500)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . # . # .
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # . # .
-        # . . . #
-        . . . . .
-        # . . . #
-        . # . # .
-        `)
-    basic.showLeds(`
-        # . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000)
 })
 let count = 0
-count = 6
+count = 1
